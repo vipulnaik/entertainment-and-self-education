b108dc315938efb6706ad5fe82f3dbc81e706434
Vipul Naik <vipulnaik1@gmail.com>
2020-12-26 15:07:43 +0000

    More summary views

--- access-portal/index.php
+++ access-portal/index.php
@@ -17,11 +17,15 @@ print '<h4>Table of contents</h4>';
 print '<ul>';
 print '<li><a href="#consumptionSummaryByPrimaryLanguageAndFormat">Consumption summary by primary language and format</a></li>';
 print '<li><a href="#consumptionSummaryByConsumptionVenueAndFormat">Consumption summary by consumption venue and format</a></li>';
+print '<li><a href="#consumptionSummaryByConsumptionVenuePrimaryLanguageAndFormat">Consumption summary by consumption venue, primary language, and format</a></li>';
+print '<li><a href="#consumptionSummaryByStartYearAndFormat">Consumption summary by start year and format</a></li>';
 print '<li><a href="#consumptionList">Consumption list</a></li>';
 print '</ul>';
 
 include ("backend/consumptionSummaryByPrimaryLanguageAndFormat.inc");
 include ("backend/consumptionSummaryByConsumptionVenueAndFormat.inc");
+include ("backend/consumptionSummaryByConsumptionVenuePrimaryLanguageAndFormat.inc");
+include ("backend/consumptionSummaryByStartYearAndFormat.inc");
 include ("backend/consumptionList.inc");
 
 include_once('anchorjs.inc');
