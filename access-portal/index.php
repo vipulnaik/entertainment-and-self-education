13b097b2a4f139e0059faf0226bafdb88de837b3
Vipul Naik <vipulnaik1@gmail.com>
2020-12-26 02:45:17 +0000

    Get a basic version of the portal ready

---
+++ access-portal/index.php
@@ -0,0 +1,24 @@
+<?php
+include_once('doctype.inc');
+print '<title>Entertainment and self-education portal for Vipul Naik</title>';
+include_once('strip-commas.inc');
+include_once('backend/stringFunctions.inc');
+print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
+print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
+print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
+print '</head>';
+include_once("backend/globalVariables/passwordFile.inc");
+print "<body>\n";
+print '<script>$(document).ready(function(){$("table").tablesorter({textExtraction: stripCommas});});</script>'."\n";
+print '<h3>Entertainment and self-education portal for Vipul Naik</h3>';
+include_once('preamble.inc');
+print '<h4>Table of contents</h4>';
+print '<ul>';
+print '<li><a href="#consumptionList">Consumption list</a></li>';
+print '</ul>';
+
+include ("backend/consumptionList.inc");
+
+include_once('anchorjs.inc');
+print '</body>';
+?>
