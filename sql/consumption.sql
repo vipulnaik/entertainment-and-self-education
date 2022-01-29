4fb61945705dc093b55a9d9111234dfb971012f7
Vipul Naik <vipulnaik1@gmail.com>
2022-01-28 19:06:31 -0800

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -22,6 +22,7 @@ create table consumption(
   - 2021-11-30 Reply 1988
   - 2022-01-03 Hwarang: The Poet Warrior Youth
   - 2022-01-26 The Devil Judge
+  - 2022-01-28 Terence Tao
  */
 
 /* MasterClass stuff */
