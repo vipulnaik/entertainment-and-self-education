378fb1698e89086ec18d6e1d113114a59f47b041
Vipul Naik <vipulnaik1@gmail.com>
2023-03-18 01:25:52 -0700

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -54,6 +54,7 @@ create table consumption(
   - 2019-05-23 - 2023-02-27 We Are Family (Hindi) (Netflix)
   - 2023-02-28 Return to Space (English) (Netflix)
   - 2023-03-03 - 2023-03-17 Money Heist Korea: Joint Economic Area (Korean) (Netflix)
+  - 2023-03-17 The Glory Part 2
  */
 
 /* MasterClass stuff */
