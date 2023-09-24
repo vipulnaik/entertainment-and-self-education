40dd2ce095d4e394d170d0e644431763043cc8ec
Vipul Naik <vipulnaik1@gmail.com>
2023-09-23 22:42:46 -0700

    Informally record a season switch

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -48,7 +48,8 @@ create table consumption(
   - 2023-08-15 The Good Wife Season 2 (Amazon Video: Freevee)
   - 2023-08-20 Silent (Japanese) (Viki)
   - 2023-09-18 The First Responders: Season 2 (Hulu)
-  - 2023-09-23 The Family Man: Season 1 (Amazon Prime)
+  - 2023-09-23 - 2023-09-23 (single day) The Family Man: Season 1 (Amazon Prime)
+  - 2023-09-23 The Family Man: Season 2 (Amazon Prime)
  */
 
 /* MasterClass stuff */
