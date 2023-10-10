7b4b5d7bdfe4c3326c09091f7a099ff0096f99f4
Vipul Naik <vipulnaik1@gmail.com>
2023-10-09 18:15:49 -0700

    Informally record a season switch

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -46,7 +46,8 @@ create table consumption(
   - 2023-07-27 One Dollar Lawyer (Hulu)
   - 2023-08-12 Tribunal Justice (Amazon Video: Freevee)
   - 2023-08-20 Silent (Japanese) (Viki)
-  - 2023-10-07 The Good Wife Season 4 (Amazon Video: Freevee)
+  - 2023-10-07 - 2023-10-09 The Good Wife Season 4 (Amazon Video: Freevee)
+  - 2023-10-09 The Good Wife Season 5 (Amazon Video: Freevee)
  */
 
 /* MasterClass stuff */
