1a1667ed2b67ace369a9f4675cbaf3e4b704abdf
Vipul Naik <vipulnaik1@gmail.com>
2023-11-09 21:41:08 -0800

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -52,6 +52,7 @@ create table consumption(
   - 2023-11-07 Jawan (Extended Cut) (Netflix)
   - 2023-11-09 - 2023-11-09 Irugapatru (Kannada audio) (Netflix)
   - 2023-11-09 Burn the House Down (Japanese) (Netflix)
+  - 2023-11-09 Doona! (Korean) (Netflix)
  */
 
 /* MasterClass stuff */
