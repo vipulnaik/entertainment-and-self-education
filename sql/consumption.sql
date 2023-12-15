02c1ab6aa7a423dc640dfd7ec0e7d38f93b821a3
Vipul Naik <vipulnaik1@gmail.com>
2023-12-14 16:13:56 -0800

    Add a start and fix the venue for a previous start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -51,7 +51,8 @@ create table consumption(
   - 2023-11-09 Doona! (Korean) (Netflix)
   - 2023-11-14 Doctor Cha (Korean) (Netflix)
   - 2023-11-24 Trillion Game (Japanese) (Netflix)
-  - 2023-12-08 A Small Light (English, mostly) (Netflix)
+  - 2023-12-08 A Small Light (English, mostly) (Hulu)
+  - 2023-12-14 The Worst of Evil (Korean) (Hulu)
  */
 
 /* MasterClass stuff */
