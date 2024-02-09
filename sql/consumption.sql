a220f8fdd4146b25d8a9006996c91b529fd1fa1c
Vipul Naik <vipulnaik1@gmail.com>
2024-02-08 23:12:30 -0800

    Informally record a consumption

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -47,6 +47,7 @@ create table consumption(
   - 2023-11-14 Doctor Cha (Korean) (Netflix)
   - 2023-12-08 A Small Light (English, mostly) (Hulu)
   - 2023-12-16 L.A. Law (English) (Hulu)
+  - 2024-02-08-2024-02-08 Sajini Shinde Ka Viral Video (Hindi) (Netflix)
  */
 
 /* MasterClass stuff */
