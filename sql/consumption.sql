e74336d29eafd30511e9c44d24309d64cf530f31
Vipul Naik <vipulnaik1@gmail.com>
2024-04-11 21:19:25 -0700

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -50,6 +50,7 @@ create table consumption(
   - 2024-03-21 Hi Nanna (Kannada) (Netflix)
   - 2024-03-23 Divorce Attorney Shin (Korean) (Netflix)
   - 2024-03-23 Doctor Slump (Korean) (Netflix)
+  - 2024-04-11 Shogun (Japanese with some English) (Hulu)
  */
 
 /* MasterClass stuff */
