493e5c8ddfea769270c9927962a950303ecd1658
Vipul Naik <vipulnaik1@gmail.com>
2024-04-14 22:21:17 -0700

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -50,6 +50,7 @@ create table consumption(
   - 2024-03-23 Divorce Attorney Shin (Korean) (Netflix)
   - 2024-03-23 Doctor Slump (Korean) (Netflix)
   - 2024-04-11 Shogun (Japanese with some English) (Hulu)
+  - 2024-04-14 ER: Season 2 (English) (Hulu)
  */
 
 /* MasterClass stuff */
