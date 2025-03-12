6e44c646cf54aaa3717320c58c1a5883fc0928a9
Vipul Naik <vipulnaik1@gmail.com>
2025-03-11 21:07:53 -0700

    Add two starts

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -64,6 +64,8 @@ create table consumption(
   - 2025-02-22 Bambai Meri Jaan (Hindi) (Amazon Prime)
   - 2025-03-09 Workin' Moms (English) (Netflix)
   - 2025-03-09 The Trauma Code: Heroes On Call (Korean) (Netflix)
+  - 2025-03-11 Aaeo (Japanese) (Netflix)
+  - 2025-03-11 Delhi Crime: Season 2 (Hindi) (Netflix)
  */
 
 /* MasterClass stuff */
