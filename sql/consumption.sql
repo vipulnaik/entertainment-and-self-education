9f943848cdab0b51d916c62423c39df9baed7b3b
Vipul Naik <vipulnaik1@gmail.com>
2025-06-13 17:27:46 -0700

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -69,6 +69,7 @@ create table consumption(
   - 2025-05-15 Mr. Queen (Korean) (Viki)
   - 2025-05-17 Usotoki Rhetoric (Japanese) (Viki)
   - 2025-05-19 Private Bodyguard (Indonesian) (Viki)
+  - 2025-06-13 Criminal Justice: A Family Matter (Kannada) (Hulu)
  */
 
 /* MasterClass stuff */
