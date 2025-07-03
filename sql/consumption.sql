d87b38dd18c4f0aa923cade1470d9fa4be33939d
Vipul Naik <vipulnaik1@gmail.com>
2025-07-03 14:42:14 -0700

    Informally record a completion

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -68,7 +68,7 @@ create table consumption(
   - 2025-05-10 Sultan of Delhi (Kannada) (Hulu)
   - 2025-05-17 Usotoki Rhetoric (Japanese) (Viki)
   - 2025-05-19 Private Bodyguard (Indonesian) (Viki)
-  - 2025-06-13 Criminal Justice: A Family Matter (Kannada) (Hulu)
+  - 2025-06-13-2025-07-03 Criminal Justice: A Family Matter (Kannada) (Hulu) (last episode watched party in Kannada but then rewatched and completed in Hindi because Kannada lacked subtitles)
   - 2025-06-24 Our Unwritten Seoul (Korean) (Netflix)
  */
 
