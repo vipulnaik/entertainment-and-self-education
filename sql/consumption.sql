64fe85d225cfc18fa473766d7dcef0035f001929
Vipul Naik <vipulnaik1@gmail.com>
2025-07-15 21:07:48 -0700

    Informally record a completion

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -59,7 +59,7 @@ create table consumption(
   - 2025-01-26 Tomorrow, I'll Be Someone's Girlfriend (Japanese) (Hulu)
   - 2025-02-06 Vigilante (Korean) (Hulu)
   - 2025-02-08 Hostel Daze: Season 1 (Hindi) (Amazon Prime)
-  - 2025-02-13 Adamas (Korean) (Hulu)
+  - 2025-02-13-2025-07-15 Adamas (Korean) (Hulu)
   - 2025-02-17 Permanent Roommates (Hindi) (Amazon Prime)
   - 2025-03-09 Workin' Moms (English) (Netflix)
   - 2025-03-16 Hyena (Korean) (Netflix)
