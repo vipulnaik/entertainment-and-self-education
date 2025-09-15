2f00faff535d7376ebcbda9569e3559d3d6db946
Vipul Naik <vipulnaik1@gmail.com>
2025-09-15 15:32:34 -0700

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -80,6 +80,7 @@ create table consumption(
   - 2025-08-22-2025-09-02 119 Emergency Call (Japanese) (Viki)
   - 2025-09-11-2025-09-13 Love Mocktail (Kannada) (Amazon Video)
   - 2025-09-13 Love Scout (Korean) (Viki)
+  - 2025-09-15 Love Mocktail 2 (Kannada) (Amazon Video)
  */
 
 /* MasterClass stuff */
