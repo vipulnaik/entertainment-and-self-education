452d37e4148be9b5544bfef32480a7b49fce7a17
Vipul Naik <vipulnaik1@gmail.com>
2025-10-26 23:15:53 -0700

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -87,6 +87,7 @@ create table consumption(
   - 2025-10-04 Second Shot At Love (Korean) (Viki)
   - 2025-10-06-2025-10-16 Blue Moment (Japanese) (Viki)
   - 2025-10-13-2025-10-19 Dum Laga Ke Haisha (Hindi) (Amazon Video)
+  - 2025-10-26 Love Birds (Kannada) (Amazon Video)
  */
 
 /* MasterClass stuff */
