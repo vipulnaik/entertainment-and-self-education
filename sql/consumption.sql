a27f874b4263afb8964d61f87a6b7e36151c0540
Vipul Naik <vipulnaik1@gmail.com>
2025-11-07 23:03:38 -0800

    Add a start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -89,6 +89,7 @@ create table consumption(
   - 2025-10-13-2025-10-19 Dum Laga Ke Haisha (Hindi) (Amazon Video)
   - 2025-10-26 Love Birds (Kannada) (Amazon Video)
   - 2025-10-28-2025-10-29 Band Baaja Baaraat (Hindi) (Amazon Video)
+  - 2025-11-07 D-Day (Korean) (Viki)
  */
 
 /* MasterClass stuff */
