8b611779513311537ca7e1c63dcce4d067db0ea6
Vipul Naik <vipulnaik1@gmail.com>
2026-05-16 21:37:48 -0700

    Add a rewatch start

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -78,6 +78,7 @@ create table consumption(
   - 2026-04-28 Misaeng (rewatch / re-listen) (Netflix)
   - 2026-05-09 ER: Season 7 (English) (Hulu)
   - 2026-05-15 Because This Is My First Life (rewatch / re-listen) (Viki)
+  - 2026-05-16 Love Birds (rewatch / re-listen) (Amazon Video)
  */
 
 /* MasterClass stuff */
