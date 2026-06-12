1b3e048399e11f6c5b6c62cb325de58fad819c6b
Vipul Naik <vipulnaik1@gmail.com>
2026-06-12 03:12:20 -0700

    Fix typo

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -84,7 +84,7 @@ create table consumption(
   - 2026-05-28 Sita Ramam (Telugu) (Amazon Prime)
   - 2026-05-29 Mischievous Kiss: Love in TOKYO (rewatch / re-listen) (Viki)
   - 2026-06-02 Healer (rewatch / re-listen) (Viki)
-  - 2026-06-11 Vyooham (Telugu) (Amazon Prine)
+  - 2026-06-11 Vyooham (Telugu) (Amazon Prime)
  */
 
 /* MasterClass stuff */
