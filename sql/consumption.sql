08afbbc8babc97398cb63a93935643e65135e965
Vipul Naik <vipulnaik1@gmail.com>
2026-06-25 14:18:05 -0700

    Informally record a completion

--- sql/consumption.sql
+++ sql/consumption.sql
@@ -82,7 +82,7 @@ create table consumption(
   - 2026-05-28 Sita Ramam (Telugu) (Amazon Prime)
   - 2026-06-02 Healer (rewatch / re-listen) (Viki)
   - 2026-06-15 (past midnight) Mischievous Kiss 2: Love in TOKYO (rewatch / re-listen) (Viki)
-  - 2026-06-17 Suzhal: The Vortex (Tamil) (Amazon Prime)
+  - 2026-06-17-2026-06-25 Suzhal: The Vortex: Season 1 (Tamil) (Amazon Prime)
  */
 
 /* MasterClass stuff */
